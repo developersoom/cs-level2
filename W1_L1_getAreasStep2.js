d3419afeae7d88ab10588238b4b71a8493f60a78
신수민 <>
2018-09-13 17:13:18 +0900

    W1_L1_Step2
    getArea 함수 구현

--- W1_L1_getAreasStep2.js
+++ W1_L1_getAreasStep2.js
@@ -1,3 +1,29 @@
+function getArea(shape, val1, val2, val3) {
+    if (shape === 'rect') {
+        let result = measureRectangleArea(val1, val2);
+        return result;
+    } else if (shape === 'trapezoid') {
+        let result = measureTrapezoidArea(val1, val2, val3);
+        return result;
+    } else if (shape === 'circle' && !val2 && !val3){
+        let result = measureCircleArea(val1);
+        return result;
+    } else if (shape === 'circle' && !val3){
+        let result = measureCirclesArea(val1, val2);
+        return result;
+    }
+}
+
+function measureCirclesArea(min, max) {
+    let p = Math.PI;
+    let areas = [];
+    for (i = min; i < max + 1; i++) {
+        areas.push(p * Math.pow(i, 2));
+    }
+    let circleAreas = areas.toString();
+    return circleAreas;
+}
+
 function measureCircleArea(r) {
     if (arguments.length !== 1) {
         console.log('error: 값이 1개가 아닙니다.');
@@ -6,25 +32,21 @@ function measureCircleArea(r) {
     } else {
         let p = Math.PI;
         let circleArea = p * Math.pow(r, 2);
-        console.log('원의 넓이는 = ' + circleArea);
+        return circleArea;
     }
 }
 
-measureCircleArea(4);
-
-
-function measureSquareArea(w, l) {
+function measureRectangleArea(w, l) {
     if (arguments.length != 2) {
         console.log('error: 값이 2개(가로,세로)가 아닙니다.');
     } else if (isNaN(w) || isNaN(l)) {
         console.log('값이 숫자가 아닙니다.');
     } else {
-        let squareArea = w * l
-        console.log('사각형의 넓이는 = ' + squareArea);
+        let rectangleArea = w * l;
+        return rectangleArea;
     }
 }
 
-measureSquareArea(3, 1);
 
 function measureTrapezoidArea(b1, b2, h) {
     if (arguments.length != 3) {
@@ -33,22 +55,6 @@ function measureTrapezoidArea(b1, b2, h) {
         console.log('error: 값이 숫자가 아닙니다.');
     } else {
         let trapezoidArea = ((b1 + b2) / 2) * h;
-        console.log('사다리꼴의 넓이는 = ' + trapezoidArea);
+        return trapezoidArea;
     }
 }
-
-measureTrapezoidArea(10, 14, 10);
-
-function measureCylinderArea(r, h) {
-    if (arguments.length != 2) {
-        console.log('error: 값이 2개(반지름, 높이)가 아닙니다.');
-    } else if (isNaN(r) || isNaN(h)) {
-        console.log('error: 값이 숫자가 아닙니다.');
-    } else {
-        let p = Math.PI
-        let cylinderArea = (2 * p * Math.pow(r, 2)) + (h * 2 * p * r);
-        console.log('원기둥의 넓이는 = ' + cylinderArea);
-    }
-}
-
-measureCylinderArea(4,10);
