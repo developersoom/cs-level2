25846fda333ab0f95d1d44bf8a1e34013f6dfabc
신수민 <>
2018-09-13 18:45:15 +0900

    printExecutionSequence() 추가

--- W1_L1_getAreasStep2.js
+++ W1_L1_getAreasStep2.js
@@ -1,21 +1,30 @@
+const logArr = [];
+
+getArea('rect',3,4);
+getArea('trapezoid',3,4,5);
+getArea('circle',3);
+getArea('circle',3,4);
+printExecutionSequence();
 
 function getArea(shape, val1, val2, val3) {
     if (shape === 'rect') {
+        logArr.push('rect');
         let result = measureRectangleArea(val1, val2);
         return console.log('사각형의 넓이는 '+result);
     } else if (shape === 'trapezoid') {
+        logArr.push('trapezoid');
         let result = measureTrapezoidArea(val1, val2, val3);
         return console.log('사다리꼴의 넓이는 '+result);
     } else if (shape === 'circle' && !val2 && !val3){
+        logArr.push('circle');
         let result = measureCircleArea(val1);
         return console.log('원의 넓이는 '+result);
     } else if (shape === 'circle' && !val3){
+        logArr.push('circle');
         let result = measureCirclesArea(val1, val2);
         return console.log('반지름 '+val1+'부터 '+val2+'까지 '+val1+'씩 증가하는 원 '+val2+'의 넓이는 '+result);
     }
 }
-
-
 
 function measureCirclesArea(min, max) {
     let p = Math.PI;
@@ -58,5 +67,11 @@ function measureTrapezoidArea(b1, b2, h) {
     } else {
         let trapezoidArea = ((b1 + b2) / 2) * h;
         return trapezoidArea;
+    }
+}
+
+function printExecutionSequence(){
+    for (i = 0; i < logArr.length; i++){
+        console.log(logArr[i]);
     }
 }
